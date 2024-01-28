12ef489bced143ed5720507a0f6eaf06471cf1fd
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:09:10 +0200

    Create vowelsSum.js

---
+++ JS Basic/For Loop - Lab/vowelsSum.js
@@ -0,0 +1,22 @@
+function vowelsSum(input) {
+    let text = input[0];
+    let sum = 0;
+
+    for (let index = 0; index < text.length; index++) {
+        let curentLetter = text[index];
+
+        switch (curentLetter) {
+            case "a": sum += 1; break; 
+            case "e": sum += 2; break; 
+            case "i": sum += 3; break; 
+            case "o": sum += 4; break; 
+            case "u": sum += 5; break; 
+        }
+    }
+    console.log(sum);
+}
+
+vowelsSum(["hello"]);
+vowelsSum(["hi"]);
+vowelsSum(["bamboo"]);
+vowelsSum(["beer"]);
