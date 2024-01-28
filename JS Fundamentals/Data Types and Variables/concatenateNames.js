420e29b301ce688d332dbda30a01f1ce29be200d
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:18:22 +0200

    Create concatenateNames.js

---
+++ JS Fundamentals/Data Types and Variables/concatenateNames.js
@@ -0,0 +1,9 @@
+function concatenateNames(name1, name2, del) {
+
+    console.log(`${name1}${del}${name2}`);
+    
+}
+
+concatenateNames('John', 'Smith', '->');
+concatenateNames('Jan', 'White', '<->');
+concatenateNames('Linda', 'Terry', '=>');
