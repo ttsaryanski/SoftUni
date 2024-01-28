997b3048b929fb37fcca7712de03116e31a138bb
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:18:40 +0200

    Create binaryToDecimal.js

---
+++ JS Fundamentals/Data Types and Variables - More Exercise/binaryToDecimal.js
@@ -0,0 +1,12 @@
+function binaryToDecimal(binary) {
+
+    let binaryNumber = binary;
+    let decimalNumber = parseInt(binaryNumber, 2);
+
+    console.log(decimalNumber);
+    
+}
+
+binaryToDecimal('00001001');
+binaryToDecimal('11110000');
+
