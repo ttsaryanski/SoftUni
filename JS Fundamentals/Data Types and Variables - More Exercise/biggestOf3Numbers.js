9fb8dc4215a23683a21d6e93cd390652a64c9612
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:18:44 +0200

    Create biggestOf3Numbers.js

---
+++ JS Fundamentals/Data Types and Variables - More Exercise/biggestOf3Numbers.js
@@ -0,0 +1,29 @@
+function biggestOf3Numbers(num1, num2, num3) {
+
+    let maxNum = 0;
+
+    if (num1 > num2 && num1 > num3) {
+        maxNum = num1;
+    } else if (num2 > num1 && num2 > num3) {
+        maxNum = num2
+    } else {
+        maxNum = num3
+    }
+
+    console.log(maxNum);
+    
+}
+
+biggestOf3Numbers(-2,
+    7,
+    3);
+biggestOf3Numbers(130,
+    5,
+    99);
+biggestOf3Numbers(43,
+    43.2,
+    43.1);
+biggestOf3Numbers(2,
+    2,
+    2);
+
