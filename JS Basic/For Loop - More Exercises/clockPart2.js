d23649089dd849315865b1459f9cc1c44a48b020
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:09:17 +0200

    Create clockPart2.js

---
+++ JS Basic/For Loop - More Exercises/clockPart2.js
@@ -0,0 +1,11 @@
+function clockPart2(input) {
+    for (let hour = 0; hour <= 23; hour++) {
+        for (let min = 0; min <= 59; min++) {
+            for (let sec = 0; sec <= 59; sec++) {
+                console.log(`${hour} : ${min} : ${sec}`);
+            }
+        }
+    }
+}
+
+clockPart2();
