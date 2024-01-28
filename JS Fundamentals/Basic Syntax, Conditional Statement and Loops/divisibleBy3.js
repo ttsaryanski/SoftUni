c3a788085f0778059d60122919c505da744243eb
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:19:26 +0200

    Create divisibleBy3.js

---
+++ JS Fundamentals/Basic Syntax, Conditional Statement and Loops/divisibleBy3.js
@@ -0,0 +1,11 @@
+function divisibleBy3() {
+
+    for (let index = 1; index <= 100; index++) {
+        if (index % 3 === 0) {
+            console.log(index);
+        }
+    }
+    
+}
+
+divisibleBy3();
