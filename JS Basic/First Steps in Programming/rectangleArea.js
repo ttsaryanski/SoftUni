450c729d2fb966ea5c675d65fb8ac93a1c411134
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-01-28 21:08:27 +0200

    Create rectangleArea.js

---
+++ JS Basic/First Steps in Programming/rectangleArea.js
@@ -0,0 +1,9 @@
+function rectangleArea(input) {
+    let a = (input[0]);
+    let b = (input[1]);
+    let rectangleArea = a * b;
+    console.log(rectangleArea);
+}
+rectangleArea(["2", "7"]);
+rectangleArea(["12", "5"]);
+rectangleArea(["7", "8"]);
