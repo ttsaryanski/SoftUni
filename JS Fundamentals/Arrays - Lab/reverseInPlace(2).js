20d392ee913c5fb5e8ce047c13a421100187a3ff
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-01 21:30:26 +0200

    Delete reverseInPlace(2).js

--- JS Fundamentals/Arrays - Lab/reverseInPlace(2).js
+++
@@ -1,7 +0,0 @@
-function reverseInPlace(arr) {
-  console.log(arr.reverse().join(" "));
-}
-
-reverseInPlace(["a", "b", "c", "d", "e"]);
-reverseInPlace(["abc", "def", "hig", "klm", "nop"]);
-reverseInPlace(["33", "123", "0", "dd"]);
