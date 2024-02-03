970173c188ae64e319a2496fe04ca7da2afb2c10
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 17:10:58 +0200

    Delete clock.js

--- JS Basic/Nested Loops - Lab/clock.js
+++
@@ -1,9 +0,0 @@
-function clock() {
-    for (let hour = 0; hour <= 23; hour++) {
-        for (let min = 0; min <= 59; min++) {
-            console.log(`${hour}:${min}`);
-        }
-    }
-}
-
-clock();
