12a984917498bfd568c7d8183421fd8a06547283
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 17:07:40 +0200

    Create personalTitles.js

---
+++ JS Basic/Conditional Statements Advanced/personalTitles.js
@@ -0,0 +1,24 @@
+function personalTitles(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+    switch (gender) {
+        case "m":
+        if (age >= 16) {
+                console.log("Mr.");
+            } else {
+                console.log("Master");
+            }
+            break;
+        case "f":        
+        if (age >= 16) {
+                console.log("Ms.");
+            } else {
+                console.log("Miss");
+            }
+            break;
+    }
+}
+personalTitles(["12", "f"]);
+personalTitles(["17", "m"]);
+personalTitles(["25", "f"]);
+personalTitles(["13.5", "m"]);
