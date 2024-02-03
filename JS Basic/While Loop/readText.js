1cd3f29812c1ee26db95d9801b36c6026d248caf
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 17:11:55 +0200

    Create readText.js

---
+++ JS Basic/While Loop/readText.js
@@ -0,0 +1,28 @@
+function readText(input) {
+    let i = 0;
+    while (input[i] !== "Stop") {
+        console.log(input[i]);
+        i++;
+    }
+    
+}
+
+// readText(["Nakov",
+// "SoftUni",
+// "Sofia",
+// "Bulgaria",
+// "SomeText",
+// "Stop",
+// "AfterStop",
+// "Europe",
+// "HelloWorld"]);
+
+readText(["Sofia",
+"Berlin",
+"Moscow",
+"Athens",
+"Madrid",
+"London",
+"Paris",
+"Stop",
+"AfterStop"]);
