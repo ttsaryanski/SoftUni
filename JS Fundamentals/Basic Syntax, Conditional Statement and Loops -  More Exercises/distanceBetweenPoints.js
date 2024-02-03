c74a39921c9e9a56948b0941ec6e88d3f729d960
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 16:59:23 +0200

    Delete distanceBetweenPoints.js

--- JS Fundamentals/Basic Syntax, Conditional Statement and Loops -  More Exercises/distanceBetweenPoints.js
+++
@@ -1,9 +0,0 @@
-function distanceBetweenpoints(x1, y1, x2, y2) {
-
-    let result = Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2);
-    console.log(Math.sqrt(result));
-    
-}
-
-distanceBetweenpoints(2, 4, 5, 0);
-distanceBetweenpoints(2.34, 15.66, -13.55, -2.9985);
