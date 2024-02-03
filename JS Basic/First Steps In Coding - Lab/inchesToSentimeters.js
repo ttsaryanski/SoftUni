4be32f087c4327527abfc25bfdcca4178fb1918a
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 17:08:31 +0200

    Delete inchesToSentimeters.js

--- JS Basic/First Steps In Coding - Lab/inchesToSentimeters.js
+++
@@ -1,8 +0,0 @@
-function inchesToSentimeters(input) {
-    let inches = Number(input[0])
-    let santimeters = inches * 2.54;
-    console.log(santimeters);
-
-}
-inchesToSentimeters([5]);
-inchesToSentimeters([7]);
