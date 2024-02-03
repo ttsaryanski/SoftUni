62c82986135a496675a5d6b605b369059242bc56
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 17:09:52 +0200

    Create characterSequence.js

---
+++ JS Basic/For Loop/characterSequence.js
@@ -0,0 +1,12 @@
+function charackterSequence(input) {
+    let text = input[0];
+    
+    for (let i = 0; i < text.length; i++) {
+        let letter = text[i];
+        console.log(letter);
+
+    }
+}
+
+charackterSequence(["softuni"]);
+charackterSequence(["ice cream"]);
