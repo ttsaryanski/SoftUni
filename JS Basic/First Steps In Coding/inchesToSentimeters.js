fcdb6375274dd8fc856dee3e7ef58ea1efd76a7b
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-03 17:08:59 +0200

    Create inchesToSentimeters.js

---
+++ JS Basic/First Steps In Coding/inchesToSentimeters.js
@@ -0,0 +1,8 @@
+function inchesToSentimeters(input) {
+    let inches = Number(input[0])
+    let santimeters = inches * 2.54;
+    console.log(santimeters);
+
+}
+inchesToSentimeters([5]);
+inchesToSentimeters([7]);
