d02db5b344ea1890750a7969a1fd4f37fc4bc2ab
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-08 21:04:49 +0200

    Create firstAndLastKNumbers.js

---
+++ JS Fundamentals/Array Advanced/firstAndLastKNumbers.js
@@ -0,0 +1,11 @@
+function firstAndLastKNumbers(array) {
+
+    let k = array.shift();
+
+    console.log(array.slice(0, k).join(' '));
+    console.log(array.slice(array.length - k, array.length).join(' '));
+
+}
+
+firstAndLastKNumbers([2, 7, 8, 9]);
+firstAndLastKNumbers([3, 6, 7, 8, 9]);
