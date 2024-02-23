8a02ac5baa3d60550234bbc750d6ec8927c05d82
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-23 07:04:45 +0200

    Create personInfo.js

---
+++ JS Fundamentals/Objects and Classes/personInfo.js
@@ -0,0 +1,15 @@
+function personInfo(firstName, secondName, age) {
+
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = secondName;
+    person.age = age;
+
+    return person;
+    
+}
+
+    
+
+personInfo("Peter", "Pan", "20");
+personInfo("George", "Smith", "18");
