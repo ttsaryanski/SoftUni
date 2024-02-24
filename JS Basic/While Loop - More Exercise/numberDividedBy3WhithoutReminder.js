46fd63bfa2169b94ba10a28c2249cfe14c8c0ffa
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-02-24 22:17:43 +0200

    Create numberDividedBy3WhithoutReminder.js

---
+++ JS Basic/While Loop - More Exercise/numberDividedBy3WhithoutReminder.js
@@ -0,0 +1,13 @@
+function numberDividedBy3WhithoutReminder() {
+
+    for (let i = 1; i <= 100; i++) {
+        let num = i;
+        if (num % 3 === 0) {
+            console.log(num);
+        }
+        
+    }
+    
+}
+
+numberDividedBy3WhithoutReminder();
