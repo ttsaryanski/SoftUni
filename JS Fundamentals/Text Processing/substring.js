15cea88909fc551fe486c1107bb4289ec28b1b31
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-03-07 19:25:05 +0200

    Create substring.js

---
+++ JS Fundamentals/Text Processing/substring.js
@@ -0,0 +1,9 @@
+function substring(word, start, count) {
+
+    let newWord = word.substring(start, start + count);
+    console.log(newWord);
+    
+}
+
+substring('ASentence', 1, 8);
+substring('SkipWord', 4, 7);
