ac211706a7d17f4407fa72633aef4e9c20116037
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-05-15 18:58:03 +0300

    Create largestNumber.js

---
+++ JS Advanced/Syntax, Function and Statements/largestNumber.js
@@ -0,0 +1,19 @@
+function largestNumber(num1, num2, num3) {
+
+    let result;
+
+    if (num1 > num2 && num1 > num3) {
+        result = num1;
+    } else if (num2 > num1 && num2 > num3) {
+        result = num2;
+    } else if (num3 > num1 && num3 > num2) {
+        result = num3;
+    }
+    
+    console.log(`The largest number is ${result}.`);
+
+
+}
+
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
