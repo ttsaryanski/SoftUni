19e073491d42a57391a4f64136e52282f00e275f
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-05-19 19:41:39 +0300

    Create printAnArrayWithAGivenDelimeter.js

---
+++ JS Advanced/Arrays and Nested Arrays - Exercise/printAnArrayWithAGivenDelimeter.js
@@ -0,0 +1,8 @@
+function printAnArrayWithAGivenDelimeter(arr, delimeter) {
+
+    console.log(arr.join(delimeter));
+    
+}
+
+printAnArrayWithAGivenDelimeter(["One", "Two", "Three", "Four", "Five"], "-");
+printAnArrayWithAGivenDelimeter(["How about no?", "I", "will", "not", "do", "it!"], "_");
