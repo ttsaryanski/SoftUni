b33879400f18bf8db4667639a9d85bdb55c4b822
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-05-19 19:41:29 +0300

    Create extractIncreasingSubsetFromArray.js

---
+++ JS Advanced/Arrays and Nested Arrays - Exercise/extractIncreasingSubsetFromArray.js
@@ -0,0 +1,15 @@
+function extractIncreasingSubsetFromArray(arr) {
+
+    let result = [];
+    let minNum = Number.MIN_SAFE_INTEGER;
+    
+
+    console.log(result);
+    return result;
+
+    
+}
+
+extractIncreasingSubsetFromArray([1, 3, 8, 4, 10, 12, 3, 2, 24]);
+extractIncreasingSubsetFromArray([1, 2, 3, 4]);
+extractIncreasingSubsetFromArray([20, 3, 2, 15, 6, 1]);
