6ae233ee8eef298d29b3862726b98aa3b4a26b25
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-05-24 19:46:40 +0300

    Create 03-sumNumbers.js

---
+++ JS Advanced/DOM Introduction/03-sumNumbers.js
@@ -0,0 +1,10 @@
+function calc() {
+
+    let num1Ref = document.getElementById("num1").value;
+    let num2Ref = document.getElementById("num2").value;
+    let sum = document.getElementById("sum");
+
+    sum.value = Number(num1Ref) + Number(num2Ref);
+    
+
+}
