99a3c4a7c08125fb5bc02e79ceb52d4d75f347c7
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-05-24 19:46:47 +0300

    Create 04-showMore.js

---
+++ JS Advanced/DOM Introduction/04-showMore.js
@@ -0,0 +1,9 @@
+function showText() {
+
+    let newStr = document.getElementById("text");
+    let target = document.getElementById("more");
+    
+    newStr.style.display = "inline";
+    target.style.display = "none";
+
+}
