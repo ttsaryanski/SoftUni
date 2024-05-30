15ea5d47744360c38bd56ff97a76377bc552685c
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-05-30 18:25:30 +0300

    Create 02-constructionCrew.js

---
+++ JS Advanced/Objects and Composition - Exercise/02-constructionCrew.js
@@ -0,0 +1,14 @@
+function constructionCrew(obj) {
+
+    if (obj.dizziness) {
+        obj.levelOfHydrated += (obj.weight * 0.1) * obj.experience;
+        obj.dizziness = false;
+    }
+
+    return obj;
+    
+}
+
+constructionCrew({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true });
+constructionCrew({ weight: 120, experience: 20, levelOfHydrated: 200, dizziness: true });
+constructionCrew({ weight: 95, experience: 3, levelOfHydrated: 0, dizziness: false });
