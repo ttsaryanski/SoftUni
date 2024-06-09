5ea445b8ca113e698d33e3699d2eb49447fe8485
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-06-09 19:32:47 +0300

    Create 05-lengthLimit.js

---
+++ JS Advanced/Classes and Attributes - Exercise/05-lengthLimit.js
@@ -0,0 +1,43 @@
+class Stringer {
+    constructor(string, length) {
+        this.innerString = string;
+        this.innerLength = length;
+    }
+
+    increase(num) {
+        this.innerLength += num;
+    }
+
+    decrease(num) {
+        if (this.innerLength - num < 0) {
+            this.innerLength = 0;
+        } else {
+            this.innerLength -= num;
+        }
+    }
+
+    toString() {
+        if (this.innerString.length > this.innerLength) {
+            return this.innerString.substring(0, this.innerLength) + '...';
+        } else if (this.innerString.length <= this.innerLength) {
+            return this.innerString;
+        } else if (this.innerLength === 0) {
+            return '...';
+        }
+
+    }
+}
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4);
+console.log(test.toString()); // Test
+
+
