1cac65bc42a1343b11b4f761c17202cdc4be4acf
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-06-09 19:32:51 +0300

    Create 07-hex.js

---
+++ JS Advanced/Classes and Attributes - Exercise/07-hex.js
@@ -0,0 +1,48 @@
+class Hex {
+    constructor(value) {
+        this.value = value;
+    } 
+
+    valueOf() {
+        return this.value;
+    } 
+
+    toString() {
+        return '0x' + this.value.toString(16).toUpperCase();
+    }
+
+    plus(num) {
+        let result;
+        if (num instanceof Hex) {
+            result = this.value + num.valueOf();
+        } else {
+            result = this.value + num;
+        }
+        return new Hex(result);
+
+    }
+
+    minus(num) {
+        let result;
+        if (num instanceof Hex) {
+            result = this.value - num.valueOf();
+        } else {
+            result = this.value - num;
+        }
+        return new Hex(result);
+    }
+
+    parse(string) {
+        return parseInt(string, 16);
+    }
+}
+
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');
+console.log(FF.parse('AAA'));
+
