e4588dfdbcd991b7147a1217f2860253b35355cb
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-06-09 09:47:35 +0300

    Create 02-getPersons.js

---
+++ JS Advanced/Classes and Attributes/02-getPersons.js
@@ -0,0 +1,25 @@
+function getPersons(firstName, lastName, age, email) {
+    
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            let result = `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    
+            return result;
+        }
+    }
+
+    return [
+        new Person ('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+        new Person ('SoftUni'),
+        new Person ('Stephan', 'Johnson', 25),
+        new Person ('Gabriel', 'Peterson', 24, 'g.p@gmail.com')
+    ]
+
+}
