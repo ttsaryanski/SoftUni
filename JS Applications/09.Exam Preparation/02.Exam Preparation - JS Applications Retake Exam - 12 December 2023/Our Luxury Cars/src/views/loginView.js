6ca52a5900614dc7ec5f42a9d2e3e81c34259253
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-07-27 10:55:58 +0300

    Create loginView.js

---
+++ JS Applications/09.Exam Preparation/02.Exam Preparation - JS Applications Retake Exam - 12 December 2023/Our Luxury Cars/src/views/loginView.js
@@ -0,0 +1,44 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { userService } from '../service/userService.js';
+import { goTo } from '../utility/goTo.js';
+import { renderer } from '../utility/render.js';
+import { createSubmitHandler } from '../utility/submiter.js';
+import { updateNav } from '../utility/updateNav.js';
+
+const temp = (handler) => html`
+    <!-- Login Page (Only for Guest users) -->
+    <section id="login">
+      <div class="form">
+        <h2>Login</h2>
+        <form @submit=${handler} class="login-form">
+          <input type="text" name="email" id="email" placeholder="email" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          <button type="submit">login</button>
+          <p class="message">
+            Not registered? <a href="/register">Create an account</a>
+          </p>
+        </form>
+      </div>
+    </section>
+`;
+
+export function showLoginView() {
+    renderer(temp(createSubmitHandler(onSubmit)));
+}
+
+async function onSubmit(data, formRef) {
+    const {email, password} = data;
+    if (!email || !password) {
+        return alert('All fields are required');
+    }
+
+    await userService.login({email, password});
+    updateNav();
+    goTo('/');
+    formRef.reset();
+}
