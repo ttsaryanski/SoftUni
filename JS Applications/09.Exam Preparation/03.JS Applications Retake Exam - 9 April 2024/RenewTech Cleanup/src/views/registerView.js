7002dce75f24cc054fd379780b6ff9b32f63c67d
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-07-28 13:34:59 +0300

    Create registerView.js

---
+++ JS Applications/09.Exam Preparation/03.JS Applications Retake Exam - 9 April 2024/RenewTech Cleanup/src/views/registerView.js
@@ -0,0 +1,53 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { renderer } from '../utility/render.js';
+import {createSubmitHandler} from '../utility/submiter.js';
+import { userService } from '../service/userService.js';
+import { goTo } from '../utility/goTo.js';
+import { updateNav } from '../utility/updateNav.js'
+
+const temp = (handler) => html`
+    <!-- Register Page (Only for Guest users) -->
+    <section id="register">
+        <div class="form">
+            <img class="border" src="./images/border.png" alt="" />
+            <h2>Register</h2>
+            <form @submit=${handler} class="register-form">
+                <input
+                    type="text"
+                    name="email"
+                    id="register-email"
+                    placeholder="email"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    id="register-password"
+                    placeholder="password"
+                />
+                <input
+                    type="password"
+                    name="re-password"
+                    id="repeat-password"
+                    placeholder="repeat password"
+                />
+                <button type="submit">register</button>
+                <p class="message">Already registered? <a href="/login">Login</a></p>
+            </form>
+        </div>
+    </section>
+`;
+
+export function showRegisterView() {
+    renderer(temp(createSubmitHandler(onSubmit)));
+}
+
+async function onSubmit(data, formRef) {
+    const {email, password, rePass = data['re-password']} = data;
+    if (!email || !password || rePass !== password) {
+        return alert('Oops');
+    }
+
+    await userService.register({email, password});
+    updateNav();
+    goTo('/');
+}
