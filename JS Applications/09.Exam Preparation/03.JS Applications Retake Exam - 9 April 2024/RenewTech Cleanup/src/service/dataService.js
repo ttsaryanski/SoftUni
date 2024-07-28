58372ff9793817a939f3de6088336176007a70f6
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-07-28 13:34:20 +0300

    Create dataService.js

---
+++ JS Applications/09.Exam Preparation/03.JS Applications Retake Exam - 9 April 2024/RenewTech Cleanup/src/service/dataService.js
@@ -0,0 +1,41 @@
+import { api } from '../utility/requester.js';
+
+const endPoints = {
+    getAll: '/data/solutions?sortBy=_createdOn%20desc',
+    createNew: '/data/solutions',
+    apiById: '/data/solutions/',
+    search: (query) => `/data/cars?where=model%20LIKE%20%22${query}%22`
+}
+
+async function getAll() {
+    return await api.get(endPoints.getAll);
+}
+
+async function createNew(data) {
+    return await api.post(endPoints.createNew, data);
+}
+
+async function getDetails(id) {
+    return await api.get(endPoints.apiById + id);
+}
+
+async function editItem(id, data) {
+    return await api.put(endPoints.apiById + id, data);
+}
+
+async function delItem(id) {
+    return await api.del(endPoints.apiById + id);
+}
+
+async function searchItem(query) {
+    return await api.get(endPoints.search(query));
+}
+
+export const dataService = {
+    getAll,
+    createNew,
+    getDetails,
+    editItem,
+    delItem,
+    searchItem
+}
