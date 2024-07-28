4a000745829a5654551221217b082bb901f2d89c
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-07-28 20:39:57 +0300

    Create likeService.js

---
+++ JS Applications/09.Exam Preparation/04.JS Applications Regular Exam - 30 March 2024/Cyberpunk Market/src/service/likeService.js
@@ -0,0 +1,33 @@
+import { api } from '../utility/requester.js';
+import { userUtil } from '../utility/userUtil.js';
+
+const endPoints = {
+    addLike: '/data/likes',
+    likesById: (itemId) => `/data/likes?where=solutionId%3D%22${itemId}%22&distinct=_ownerId&count`,
+    likesByUser: (itemId, userId) => `/data/likes?where=solutionId%3D%22${itemId}%22%20and%20_ownerId%3D%22${userId}%22&count`
+}
+
+async function postLike(data) {
+    return await api.post(endPoints.addLike, data);
+}
+
+async function getLikesById(id) {
+    const userData = userUtil.getUserData();
+
+    const requests = [
+        api.get(endPoints.likesById(id))
+    ];
+
+    if (userData) {
+        requests.push(api.get(endPoints.likesByUser(id, userData._id)));
+    }
+
+    const [likes, hasLiked] = await Promise.all(requests);
+    
+    return { likes, hasLiked: Boolean(hasLiked)};
+}
+
+export const likesService = {
+    postLike,
+    getLikesById
+}
