eaab8beb33faa09e0314fd90004ec2a5675cf9c5
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-07-31 20:43:30 +0300

    Create app.js

---
+++ JS Applications/09.Exam Preparation/05.JS Applications Regular Exam - 29 July 2023/Fun Facts/src/app.js
@@ -0,0 +1,25 @@
+import page from '../node_modules/page/page.mjs';
+import { logout } from './utility/logout.js';
+import { updateNav } from './utility/updateNav.js';
+import { showCreateView } from './views/createView.js';
+import { showDashboardView } from './views/dashboardView.js';
+import { showDetailsView } from './views/detailsView.js';
+import { showEditView } from './views/editView.js';
+import { showHomeView } from './views/homeView.js';
+import { showLoginView } from './views/loginView.js';
+import { showRegisterView } from './views/registerView.js';
+
+
+showHomeView();
+page('/', showHomeView);
+page('/dashboard', showDashboardView);
+page('/register', showRegisterView);
+page('/login', showLoginView);
+page('/logout', logout);
+page('/creat', showCreateView);
+page('/details/:id', showDetailsView);
+page('/edit/:id', showEditView)
+page('/search', () => console.log('search'));
+
+page.start();
+updateNav();
