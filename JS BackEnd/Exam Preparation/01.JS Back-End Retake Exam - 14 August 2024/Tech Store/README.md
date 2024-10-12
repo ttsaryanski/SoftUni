7d5bc7c7f881948f551befa20e3f9acff9c9994a
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-10-12 19:02:26 +0300

    Move to other repo

--- JS BackEnd/Exam Preparation/01.JS Back-End Retake Exam - 14 August 2024/Tech Store/README.md
+++
@@ -1,3 +0,0 @@
-# JS Back-End Exam preparation – TechStore
-
-The information found in constans.js is visible for the purpose of testing the application!!!
--- JS BackEnd/Exam Preparation/01.JS Back-End Retake Exam - 14 August 2024/Tech Store/src/config/handlebarsInit.js
+++
@@ -1,9 +0,0 @@
-import handlebars from 'express-handlebars';
-
-export default function handlebarsInit (app) {
-    app.engine('hbs', handlebars.engine({
-        extname: 'hbs'
-    }));
-    app.set('view engine', 'hbs');
-    app.set('views', './src/views');
-};
